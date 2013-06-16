2eab9124a51eef26c26fc16606b09f5eac8ffd01
Emu Mantoid <stimularity@gmail.com>
2013-06-15 23:04:09 -0600

    Updated to handle all sorts of input sizes or adjust dynamically.

--- lightbox.js
+++ lightbox.js
@@ -1,29 +1,40 @@
-		function lightBox(w, title, description, content) {
+	//Max's custom animated lightbox plugin. Quite magical.
+	function lightBox(w, h, content) {
+		$('#lightbox, #lightboxcore').remove(); //Destroy old lightboxes. There can only be one.
+		$('body').prepend('<div id="lightboxcore">'+content+'</div>'); //Append inner box
 
-			innerdata = '<div class="hoverboxtitle">' + title + '</div><div class="hoverboxtext">' + description + '</div><div class="spacer"><div class="badgebox">' + content + '</div></div>';
+		//Default values if none are set.
+		marginsize = 200;
+		width = $(window).width() - marginsize; 
+		height = $(window).height() - marginsize;
 
-			$('#lightbox, #lightboxcore').remove(); //Destroy old lightboxes. There can only be one.
-
-			$('body').prepend('<div id="lightboxcore">'+innerdata+'</div>'); //Append inner box
-			h = $('#lightboxcore').height(); //Get Height of box used for centering, height is set dynamically.
-			$('#lightboxcore').css({'width': w}); //Set width of lightboxcore
-
-			$('html').prepend('<div id="lightbox"></div>').fadeIn(400); //Add outer box
-			$('#lightbox').css({'height': $(document).height(), 'width': $(document).width()}); //Set size of backdrop
-			$("#lightbox").click(function() {
-				closeLightbox(); //Bind close event to backdrop
-			});
-
-			//Center the box on page
-			$('#lightboxcore').css("top", (($(window).height() - $('#lightboxcore').outerHeight()) / 2) + $(window).scrollTop() + "px");
-			$('#lightboxcore').css("left", (($(window).width() - $('#lightboxcore').outerWidth()) / 2) + $(window).scrollLeft() + "px");
-			
-			$('#lightboxcore').hide().fadeIn(500); //Fade in core
-			enableAwesomeToolTips(); //Enable Tool tips for visible badges
+		if(w != 0 && h == 0){ //Width is set, but not height, get height from content.	
+			height = $('#lightboxcore').height(); //Get Height of box used for centering, height is set dynamically.
+			width = w;
 		}
-		function closeLightbox() {
-			$('#lightbox').fadeOut(500);
-			$('#lightboxcore').fadeOut(300);
-			$('#hoverbox').fadeOut(300); //Hover boxes also get killed No lingering boxes.
-			disableAwesomeToolTips(); //Turns off tool tips on lightbox close
+		if(w == 0 && h != 0){ //Width is set, use default height. 
+			height = h;
 		}
+		if(w != 0 && h != 0){ //Use user inputs. 
+			width = w;
+			height = h;
+		}
+		
+		$('#lightboxcore').css({'width': width, 'height':height}); //Set width of lightboxcore
+
+		//Add backdrop to DOm
+		$('html').prepend('<div id="lightbox"></div>').fadeIn(400); 
+		$('#lightbox').css({'height': $(document).height(), 'width': $(document).width()}); //Set proper size
+		$("#lightbox").click(function() {
+			closeLightbox(); //Bind close event to backdrop
+		});
+		//Center the core on page
+		$('#lightboxcore').css("top", (($(window).height() - $('#lightboxcore').outerHeight()) / 2) + $(window).scrollTop() + "px");
+		$('#lightboxcore').css("left", (($(window).width() - $('#lightboxcore').outerWidth()) / 2) + $(window).scrollLeft() + "px");
+		//Make it visible. 
+		$('#lightboxcore').hide().fadeIn(500);
+	}
+	function closeLightbox(){
+		$('#lightbox').fadeOut(500);
+		$('#lightboxcore').fadeOut(300);
+	}
